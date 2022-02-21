7bc6c01dfbe63d4f5067737df97bf79dc3b6df0f
georgebanlaki <george.banlaki@gmail.com>
2022-02-21 23:21:08 +0000

    scrollto

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ $("#scrolldown").click(function () {
     "linear"
   );
 });
+
+$("#heroNav").click(function () {
+  $("html, body").animate(
+    { scrollTop: $("#hero").offset().top },
+    1000,
+    "linear"
+  );
+});
+
+$("#projectsNav").click(function () {
+  $("html, body").animate(
+    { scrollTop: $("#projects").offset().top },
+    1000,
+    "linear"
+  );
+});
+
+$("#skillsNav").click(function () {
+  $("html, body").animate(
+    { scrollTop: $("#skills").offset().top },
+    1000,
+    "linear"
+  );
+});
+
+$("#contactNav").click(function () {
+  $("html, body").animate(
+    { scrollTop: $("#contact").offset().top },
+    1000,
+    "linear"
+  );
+});
